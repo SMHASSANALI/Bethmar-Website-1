5b20168493d2f1ff9912bf3a84f704d913cac4da
AbdulHannan003 <terminator.zed2018@gmail.com>
2024-08-05 19:46:50 +0500

    404 Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -19,6 +20,7 @@ function App() {
         <ScrollToTop />
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/*" element={<NotFound />} />
         </Routes>
       </Router>
     </>
